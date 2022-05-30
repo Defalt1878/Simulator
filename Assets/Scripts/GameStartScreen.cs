a3201c71158b29ddd6025c432cbd912b5ce96824
Defalt1878 <ilyaudalov1@mail.ru>
2022-05-30 10:59:06 +0500

    Добавлен экран загрузки и анимация загрузки в бразузере

---
+++ Assets/Scripts/GameStartScreen.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UserData;
+
+public class GameStartScreen : MonoBehaviour
+{
+	private Task _loadingTask;
+	private const float LoadingScreenMinTime = 2f;
+
+	private void Awake()
+	{
+		_loadingTask = Task.Run(StaticData.GetInstance);
+		Invoke(nameof(StartGame), LoadingScreenMinTime);
+	}
+
+	private void StartGame()
+	{
+		if (!_loadingTask.IsCompleted)
+		{
+			Invoke(nameof(StartGame), 0.5f);
+			return;
+		}
+
+		gameObject.SetActive(false);
+	}
+}
